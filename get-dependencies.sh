fa85ddb7e18e71ae02a7beaefb21b256b887c33b
christian <christian@risu-debian.sarre.eu>
2025-12-05 13:20:54 +0200

    Revert "Test build with nano-packages disabled"
    This reverts commit 0b78ac6e8305d07eefe34eefc33678ee6c68581c.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm prusa-slicer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common
+get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
