7aa7ecb0c40d90823ca75ef9bdb35e8b405599a8
sarrchri <christian@sarre.eu>
2025-12-09 17:28:53 +0200

    Install 'nss-mdns' during build.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm prusa-slicer
+pacman -Syu --noconfirm prusa-slicer nss-mdns
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
