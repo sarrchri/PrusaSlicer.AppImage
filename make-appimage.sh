b4f03185abd8cc14ef3b90c097026c4e5eaa6b79
sarrchri <christian@sarre.eu>
2026-04-30 22:46:51 +0300

    Rename hook.

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q prusa-slicer | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/192x192/apps/PrusaSlicer.png
 export DESKTOP=/usr/share/applications/PrusaSlicer.desktop
